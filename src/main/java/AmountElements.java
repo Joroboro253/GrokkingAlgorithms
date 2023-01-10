8d5910cbce31f60334aec8f5aea2fc83ba70f9d6
Vitaliy Lyashchenko <vlyashchenko01@gmail.com>
2023-01-10 18:50:25 +0200

    Shit

--- src/main/java/AmountElements.java
+++ src/main/java/AmountElements.java
@@ -1,13 +1,44 @@
 //рекурсивная функция для подсчета элементов в списке
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class AmountElements {
-    public int amount(int lengthArray, int array[]){
-        if(lengthArray <= 0){
-            return 0;
-        } else {
-            int numberSum = array[lengthArray-1];
-            return 0;
-        }
-    }
+    public int amount(ArrayList<Integer> array){
+        //List<Integer> newArr = new ArrayList<>();
+
+//        for (int i: array) {
+//            newArr.add(Integer.valueOf(i));
+//        }
+
+
+
+//        int i = 0;
+//        if(newArr.get(i) == null){
+//            return 0;
+//        } else {
+//            i++;
+//
+//            return amount(array);
+//        }
+
+
+
+//        int elements = 0;
+//        if(array.length == 0){
+//            return 0;
+//        } else {
+//
+//            elements++;
+//            int myArray = amount(array);
+//            return elements;
+//        }
+   }
+
+
+
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int[] array = {5, 10, 15, 13, 5};
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(4);
+        arr.add(5);
+        arr.add(2);
+        arr.add(23);
+        arr.add(55);
         //array =
-        Summator sm = new Summator();
+        //Summator sm = new Summator();
+        AmountElements am = new AmountElements();
 
-
-        System.out.println(sm.sum(array.length, array));
+        System.out.println(am.amount(arr));
+       // System.out.println(sm.sum(array.length, array));
 
     }
 
